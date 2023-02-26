608097b2b3f0f5d4c6d00043882a8a2e2ec0f690
Cryxyemi <maiks0809@gmail.com>
2023-02-26 19:08:22 +0100

    Update example

--- example/basic_bot.py
+++ example/basic_bot.py
@@ -11,7 +11,4 @@ async def ping(ctx: discord.ApplicationContext) -> None:
     await ctx.respond("pong")
 
 if __name__ == "__main__":
-    bot.load_cogs("cogs")  # Load all cogs in the "cogs" folder
-    bot.load_subdir("commands")  # Load all cogs in the "commands" folder and all subfolders
-
     bot.exec()
