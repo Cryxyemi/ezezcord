87bc288fa7d96c039e4e6e247c852183bfd54fa1
Cryxyemi <maiks0809@gmail.com>
2023-03-01 19:40:33 +0100

    fixed version checker

--- src/mikocord/utils/ver_check.py
+++ src/mikocord/utils/ver_check.py
@@ -15,7 +15,10 @@ class version:
                 text = await resp.text()
                 github_version = text.split("__version__ = ")[1].split("\n")[0].replace('"', "")
 
-        if float(current_version) >= float(github_version):
+        current_version = current_version.replace(".", "")
+        github_version = github_version.replace(".", "")
+
+        if int(current_version) >= int(github_version):
             return log._force_logger("You are using the latest version of mikocord.", "version", "info")
         else:
             return log._force_logger("You are using an outdated version of mikocord. Please update to the latest version.", "version", "warning")
