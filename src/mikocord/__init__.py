1d346adbe9a15eab011474fa6adcca9bb5c46821
Cryxyemi <maiks0809@gmail.com>
2023-03-01 19:24:19 +0100

    Fixed version checker

--- src/mikocord/__init__.py
+++ src/mikocord/__init__.py
@@ -1,9 +1,9 @@
 __title__ = "mikocord"
 __author__ = "Cryxyemi"
 __license__ = "MIT"
-__version__ = "1.7"
+__version__ = "1.8.5"
 
 from .bot import Bot
-from .utils.ver_check import _check_version
+from .utils.ver_check import version
 
-_check_version(__version__)
+version._check(__version__)
--- src/mikocord/utils/ver_check.py
+++ src/mikocord/utils/ver_check.py
@@ -1,21 +1,32 @@
 import aiohttp
 import asyncio
+import threading
 
 from .log import Log
 
 
-async def _get_version(current_version: str) -> bool:
-    log = Log(debug=False, with_date=False)
+class version:
+    @classmethod
+    async def _get_version(cls, current_version: str) -> bool:
+        log = Log(debug=False, with_date=False)
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get("https://raw.githubusercontent.com/Cryxyemi/mikocord/main/src/mikocord/__init__.py") as resp:
-            text = await resp.text()
-            github_version = text.split("__version__ = ")[1].split("\n")[0].replace('"', "")
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://raw.githubusercontent.com/Cryxyemi/mikocord/main/src/mikocord/__init__.py") as resp:
+                text = await resp.text()
+                github_version = text.split("__version__ = ")[1].split("\n")[0].replace('"', "")
 
-    if float(current_version) >= float(github_version):
-        return log._force_logger("You are using the latest version of mikocord.", "version", "info")
-    else:
-        return log._force_logger("You are using an outdated version of mikocord. Please update to the latest version.", "version", "warning")
+        if float(current_version) >= float(github_version):
+            return log._force_logger("You are using the latest version of mikocord.", "version", "info")
+        else:
+            return log._force_logger("You are using an outdated version of mikocord. Please update to the latest version.", "version", "warning")
 
-def _check_version(current_version: str) -> None:
-    asyncio.run(_get_version(current_version))
+    @classmethod
+    def __check_version(cls, current_version: str) -> None:
+        asyncio.run(cls._get_version(current_version))
+
+    @classmethod
+    def _check(cls, current_version: str) -> None:
+        thread = threading.Thread(target=cls.__check_version, args=(current_version,))
+
+        thread.start()
+        thread.join()
